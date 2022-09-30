29ab541e460a3738973638a9f2b0d48e2d67638c
Murilo Alves Melo <murilo.melo72@gmail.com>
2022-09-30 14:35:07 -0300

    Add URL Heroku

--- knexfile.js
+++ knexfile.js
@@ -3,14 +3,12 @@
 /**
  * @type { Object.<string, import("knex").Knex.Config> }
  */
-module.exports = {
 
-    client: 'postgresql',
-    connection: {
-      database: 'tasks',
-      user:     'postgres',
-      password: '1234'
-    },
+DATABASE_URL = 'postgres://rzbhtqrehlkvtm:16920f3f20a61d6d2d60f2430a96ebd250ff968a266d6c1cc242a2fe74b9cd3a@ec2-3-93-206-109.compute-1.amazonaws.com:5432/dbdsh9ues0kt3l'
+
+module.exports = {
+    client: 'pg',
+    connection: 'postgres://rzbhtqrehlkvtm:16920f3f20a61d6d2d60f2430a96ebd250ff968a266d6c1cc242a2fe74b9cd3a@ec2-3-93-206-109.compute-1.amazonaws.com:5432/dbdsh9ues0kt3l',
     pool: {
       min: 2,
       max: 10
