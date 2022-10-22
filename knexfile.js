f4ebf44ca6158196647364cfb4dac8597a0c15f6
murilo-alvesmelo <murilo.melo72@gmail.com>
2022-10-22 13:40:02 -0300

    up heroku api

--- knexfile.js
+++ knexfile.js
@@ -10,7 +10,7 @@ module.exports = {
       database: process.env.DATABASE,
       user: process.env.USER,
       password: process.env.PASSWORD,
-      port: 5432,
+      port: process.env.PORT,
     },
     pool: {
       min: 2,
