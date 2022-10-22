526847fcb71f3d017845df0c38ef54a5ce229f87
murilo-alvesmelo <murilo.melo72@gmail.com>
2022-10-22 16:18:18 -0300

    up heroku api

--- knexfile.js
+++ knexfile.js
@@ -23,7 +23,10 @@
   production:{
     client: 'pg',
     connection: process.env.DATABASE_URL,
-    ssl: true,
+    pool: {
+      min: 2,
+      max: 10
+    },
     migrations: {
       diectory: __dirname + '/migrations',
     },
